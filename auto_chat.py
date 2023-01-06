96001c8a53f23a907c89dcd2e6fc9312533d13ef
Hamada-Gado <ahmedgado0100@gmail.com>
2023-01-06 22:22:13 +0200

    refactor open_app function
    now try to find window in windows list for
    a minSearchTime intialy set to 5 if not found return
    from function
    
    TODO raise error when not found and handle it

--- auto_chat.py
+++ auto_chat.py
@@ -1,12 +1,29 @@
-import subprocess, pyautogui as pag
+import subprocess, time, pyautogui as pag
+from pygetwindow import Window
+from pathlib import Path
 
-WHATS_APP_PATH              = r'C:\"Program Files"\WindowsApps\5319275A.WhatsAppDesktop_2.2248.9.0_x64__cv1g1gvanyjgm\WhatsApp.exe'
-WHATS_APP_LOGO_IMAGE_PATH   = "WhatsApp Logo.png" 
+WHATSAPP_PATH   : Path  = Path('C:/Program Files/WindowsApps/5319275A.WhatsAppDesktop_2.2248.9.0_x64__cv1g1gvanyjgm/WhatsApp.exe')
+WHATSAPP_WINDOW : Window
 
-def open_app():
-    subprocess.run(f'start "" {WHATS_APP_PATH}', shell = True)
-    pag.locateOnScreen(image= WHATS_APP_LOGO_IMAGE_PATH, minSearchTime= 5) # make sure that the window opened before maximizing it to not throw an error
-    pag.getWindowsWithTitle("WhatsApp")[0].maximize() # type: ignore
+def open_app(minSearchTime= 5) -> None:
+    global WHATSAPP_WINDOW
+    
+    subprocess.run(['start', "", WHATSAPP_PATH], shell = True)
+    start_time = time.time()
+
+    while time.time() - start_time < minSearchTime:
+        try:
+            WHATSAPP_WINDOW = pag.getWindowsWithTitle("WhatsApp")[0] # type: ignore
+        except IndexError:
+            continue
+        break
+    try:
+        assert(WHATSAPP_WINDOW)
+    except NameError:
+        return # TODO raise error if can't open the app
+    else:
+        WHATSAPP_WINDOW.activate()
+        WHATSAPP_WINDOW.maximize()
     
 def main():
    open_app() 
