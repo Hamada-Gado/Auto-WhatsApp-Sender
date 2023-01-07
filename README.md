57f03841b86489f305fc235c5c81aa30b056a376
Hamada-Gado <ahmedgado0100@gmail.com>
2023-01-07 22:28:38 +0200

    can send text messages now

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 * WhatsApp version used = 2.2248.9.0
 * Python version used = 3.11.1
+
+```
+Enter pip install -r requirements.txt before using
+```
--- auto_chat.py
+++ auto_chat.py
@@ -1,11 +1,19 @@
-import subprocess, time, pygetwindow as pgw, pyautogui as pag
+import subprocess
+import time
 from pathlib import Path
 from tkinter import messagebox
+from typing import Generator
+
+import pyautogui as pag
+import pygetwindow as pgw
+
 from Exceptions import *
 
 WHATSAPP_PATH   : Path  = Path('C:/Program files/WindowsApps/5319275A.WhatsAppDesktop_2.2248.9.0_x64__cv1g1gvanyjgm/WhatsApp.exe')
 WHATSAPP_TITLE  : str   = "WhatsApp"
 WHATSAPP_WINDOW : pgw.Window
+NAMES : list[str] = ["Ahmed Hisham", "Yousef Yasser", "Sawy"]
+MESSAGE : str = "bagarb 7aga 3ady dagahl el resala de"
 
 def open_app(minSearchTime= 5) -> None:
     global WHATSAPP_WINDOW
@@ -36,17 +44,36 @@ def open_app(minSearchTime= 5) -> None:
         pass
     
     WHATSAPP_WINDOW.maximize()
+    time.sleep(minSearchTime) # wait to make sure the application is ready for shortcuts
+
+def send(name: str, message: str, outer_interval= 0.5, internal_interval: float = 0.1) -> None:
+    def instructions() -> Generator[None, None, None]:
+        yield pag.hotkey("ctrl", "a", interval= internal_interval)
+        yield pag.press("backspace", interval= internal_interval)
+        yield pag.hotkey("ctrl", "f", interval= internal_interval)
+        yield pag.typewrite(name, interval= internal_interval)
+        yield pag.hotkey("ctrl", "1", interval= internal_interval)
+        yield pag.typewrite(message, interval= internal_interval)
+        yield pag.press("enter")
+        
+    for instruction in instructions():
+        time.sleep(outer_interval)
     
-def main() -> int:
+def send_all():
+    pag.hotkey("ctrl", "1")
+    for name in NAMES:
+        send(name, MESSAGE)
+    
+def main() -> None:
     try:
         open_app() 
-    except subprocess.CalledProcessError:
-        return 1
+    except subprocess.CalledProcessError: # the os handles it
+        return
     except WindowNotFoundException as e:
         messagebox.showerror(title= "Window not found", message= str(e))
-        return 2
-    else:
-        return 0
+        return
+        
+    send_all()
         
 if __name__ == "__main__":
     main()
