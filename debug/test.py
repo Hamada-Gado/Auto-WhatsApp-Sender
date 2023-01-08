3176ee978d2c2017b9f7ff7b47c6cc1fbec6f77f
Hamada-Gado <ahmedgado0100@gmail.com>
2023-01-08 17:17:43 +0200

    fix .gitignore entered wrong name first time

--- debug/test.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-
-NAMES : list[str] = ["Ahmed Hisham", "Yousef Yasser", "Sawy"]
-MESSAGE : str = "bagarb 7aga 3ady dagahl el resala de"
-IMAGE : Image.Image = Image.open("F:/GitHub Repos/Auto-WhatsApp-Sender/debug/WhatsApp Logo.png")
